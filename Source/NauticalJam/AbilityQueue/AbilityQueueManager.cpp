dedf193e2bd485cadae89090849799caa083fba1
Alirie Gray <alirie.gray@gmail.com>
2026-07-22 14:36:38 -0700

    Fix compile errors

--- Source/NauticalJam/AbilityQueue/AbilityQueueManager.cpp
+++ Source/NauticalJam/AbilityQueue/AbilityQueueManager.cpp
@@ -1,2 +1,26 @@
-﻿#include "C:\Users\Alirie\Documents\Unreal Projects\NauticalJam\Intermediate\Build\Win64\x64\NauticalJamEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.ValExpApi.Cpp20.h"
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
 #include "AbilityQueueManager.h"
+
+
+// Sets default values
+AAbilityQueueManager::AAbilityQueueManager()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AAbilityQueueManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAbilityQueueManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
--- Source/NauticalJam/AbilityQueue/AbilityQueueManager.h
+++ Source/NauticalJam/AbilityQueue/AbilityQueueManager.h
@@ -1,7 +1,25 @@
-﻿#pragma once
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-class AbilityQueueManager
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AbilityQueueManager.generated.h"
+
+UCLASS()
+class NAUTICALJAM_API AAbilityQueueManager : public AActor
 {
+	GENERATED_BODY()
+
 public:
-	
+	// Sets default values for this actor's properties
+	AAbilityQueueManager();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 };
